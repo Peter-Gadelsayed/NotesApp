04ea2a886ac8c310c5b72d6fcd360d02d2fd1062
Peter-Gadelsayed <peter.gadelsayed@outlook.com>
2025-04-02 21:50:26 +0200

    initial commit

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { LogInComponent } from './authantications/log-in/log-in.component';
+import { SignUpComponent } from './authantications/sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  declarations: [
+    LogInComponent,
+    SignUpComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ]
+})
+export class CoreModule { }
