35430c326f2c03f58e22a463dbe847cb4c60799b
Mahmoud-ahmed22 <126742943+Mahmoud-ahmed22@users.noreply.github.com>
2025-04-03 10:13:57 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'landing', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'landing', component: LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
 import { CoreModule } from './core/core.module';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
