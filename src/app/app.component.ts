713952b0488771fef037bfd124f61c2903f533f8
Mahmoud-ahmed22 <126742943+Mahmoud-ahmed22@users.noreply.github.com>
2025-04-03 04:40:25 +0200

    test1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from "./shared/nav-bar/nav-bar.component";
+import { HomeComponent } from "./core/home/home.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'notes-app';
