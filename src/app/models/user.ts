e1cc541d64755ea25380bca6034c97e2b7e36131
Peter-Gadelsayed <peter.gadelsayed@outlook.com>
2025-04-05 14:26:51 +0200

    signup

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+  phone: string;
+  age: string;
+  address: string;
+}
+
+export interface Credentials {
+  email: string;
+  password: string;
+}
