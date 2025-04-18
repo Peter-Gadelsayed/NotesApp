5aaa4cc7d8906872df7f79fb5ad975bbe30fbbbe
shehab hosny mohamed <93824411+shehab125@users.noreply.github.com>
2025-04-18 15:31:03 +0200

    Update landing.component.ts

--- src/app/core/landing/landing.component.ts
+++ src/app/core/landing/landing.component.ts
@@ -10,6 +10,24 @@ import { Router } from '@angular/router';
 export class LandingComponent {
   constructor(private router: Router, private authService: AuthService) { }
 
+  ngOnInit(): void {
+    // smooth scrolling
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = (e.currentTarget as HTMLAnchorElement).getAttribute('href');
+        if (targetId) {
+          const targetElement = document.querySelector(targetId);
+          if (targetElement) {
+            targetElement.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start'
+            });
+          }
+        }
+      });
+    });
+  }
   navigateToCreate() {
     if (this.authService.isLogged()) {
       this.router.navigate(['/notes/create']);
